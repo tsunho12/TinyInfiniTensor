db153f8a0550ccb8d40f49cc3edf1c3c02eda43c
tsunho12 <nancy145263@gmail.com>
2025-01-15 21:02:37 +0800

    完成作业六：双向广播

--- src/utils/operator_utils.cc
+++ src/utils/operator_utils.cc
@@ -1,69 +1,82 @@
 #include "utils/operator_utils.h"
+
 #include "core/runtime.h"
 
 namespace infini {
 
 Shape infer_broadcast(const Shape &A, const Shape &B) {
-
-    // =================================== 作业 ===================================
-    // TODO：对 A 和 B 进行双向广播，返回广播后的形状。
-    // REF: https://github.com/onnx/onnx/blob/main/docs/Broadcasting.md
-    // =================================== 作业 ===================================
-    
-    return {};
+  // =================================== 作业
+  // ===================================
+  // TODO：对 A 和 B 进行双向广播，返回广播后的形状。
+  // REF: https://github.com/onnx/onnx/blob/main/docs/Broadcasting.md
+  // =================================== 作业
+  // ===================================
+  Shape result(std::max(A.size(), B.size()), 1);
+  for (int i = 0; i < static_cast<int>(result.size()); ++i) {
+    int A_index{static_cast<int>(A.size()) - i - 1};
+    int B_index{static_cast<int>(B.size()) - i - 1};
+    if (A_index >= 0 && B_index >= 0) {
+      result.at(result.size() - i - 1) = std::max(A.at(A_index), B.at(B_index));
+    } else if (A_index >= 0) {
+      result.at(result.size() - i - 1) = A.at(A_index);
+    } else {
+      result.at(result.size() - i - 1) = B.at(B_index);
+    }
+  }
+  return result;
 }
 
 int get_real_axis(const int &axis, const int &rank) {
-    IT_ASSERT(rank >= 1);
-    IT_ASSERT(axis >= -rank && axis <= (rank - 1));
-    int newAxis;
-    if (axis < 0) {
-        newAxis = rank + axis;
-    } else {
-        newAxis = axis;
-    }
-    return newAxis;
+  IT_ASSERT(rank >= 1);
+  IT_ASSERT(axis >= -rank && axis <= (rank - 1));
+  int newAxis;
+  if (axis < 0) {
+    newAxis = rank + axis;
+  } else {
+    newAxis = axis;
+  }
+  return newAxis;
 }
 
 Shape locate_index(size_t inputN, const Shape &shape) {
-    Shape ans(shape.size());
-    auto i = ans.rbegin();
-    auto j = shape.rbegin(), ej = shape.rend();
-    while (j != ej) {
-        auto div = std::div(inputN, *j++);
-        *i++ = div.rem;
-        inputN = div.quot;
-    }
-    return ans;
+  Shape ans(shape.size());
+  auto i = ans.rbegin();
+  auto j = shape.rbegin(), ej = shape.rend();
+  while (j != ej) {
+    auto div = std::div(inputN, *j++);
+    *i++ = div.rem;
+    inputN = div.quot;
+  }
+  return ans;
 }
 
 size_t delocate_index(const Shape &shapeIndex, const Shape &shape,
                       const Shape &stride) {
-    size_t ans = 0;
-    Shape index(shapeIndex.size());
-    IT_ASSERT(shapeIndex.size() == shape.size());
-    IT_ASSERT(shape.size() == stride.size());
-    for (size_t i = 0; i < shape.size(); ++i) {
-        index[i] = shapeIndex[i] % shape[i];
-        ans += index[i] * stride[i];
-    }
-    return ans;
+  size_t ans = 0;
+  Shape index(shapeIndex.size());
+  IT_ASSERT(shapeIndex.size() == shape.size());
+  IT_ASSERT(shape.size() == stride.size());
+  for (size_t i = 0; i < shape.size(); ++i) {
+    index[i] = shapeIndex[i] % shape[i];
+    ans += index[i] * stride[i];
+  }
+  return ans;
 }
 
 std::string device_to_str(Device device) {
-    std::string deviceStr;
-    switch (device) {
+  std::string deviceStr;
+  switch (device) {
     case Device::CPU:
-        return "CPU";
+      return "CPU";
     default:
-        IT_TODO_HALT();
-    }
+      IT_TODO_HALT();
+  }
 }
 
 std::string get_kernel_attrs_str(const KernelAttrs &kernelAttrs) {
-    std::string deviceStr = device_to_str(std::get<0>(kernelAttrs));
-    std::string opStr = OpType(std::get<1>(kernelAttrs)).toString();
-    return deviceStr + ", " + opStr;
+  std::string deviceStr = device_to_str(std::get<0>(kernelAttrs));
+  std::string opStr = OpType(std::get<1>(kernelAttrs)).toString();
+  return deviceStr + ", " + opStr;
 }
 
-} // namespace infini
+}  // namespace infini
